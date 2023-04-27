89d2228125f1fd51aba699af356143854d0c5625
shubhamak9 <127405459+shubhamak9@users.noreply.github.com>
2023-04-27 23:52:47 +0530

    day5 just minimum no. of questin solved

---
+++ day5/p8_Missing_number.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int missingNumber(int a[], int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int i=0, n;
+		cin>>n;
+		int a[n+5];
+		for(i=0;i<n-1;i++)
+			cin>>a[i];
+			
+		cout<<missingNumber(a, n)<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+int missingNumber(int arr[], int n)
+{
+    // Your code goes here
+    int sum = accumulate(arr, arr+n-1, 0);
+    return n*(n+1)/2-sum;
+}
